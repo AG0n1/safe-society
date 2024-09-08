f21db5e84b38974eab762997f770150086c1f9b5
Matthew Markovets <mmarkovec15072003@gmail.com>
2024-09-08 23:56:31 +0300

    Added generateKey func

--- src/assets/Helpers/actions.tsx
+++ src/assets/Helpers/actions.tsx
@@ -2,6 +2,16 @@ import { Button } from "antd"
 import { IHeaderButtons } from "../types/HeadetInterfaces"
 import { useNavigate } from 'react-router-dom'
 
+export const generateKey = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let key = '';
+    for (let i = 0; i < 10; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        key += characters[randomIndex];
+    }
+    return key;
+}
+
 export const generateButtons = (buttonsArray: IHeaderButtons[]) => {
 
     const navigate = useNavigate()
@@ -14,21 +24,21 @@ export const generateButtons = (buttonsArray: IHeaderButtons[]) => {
         switch (button.type) {
             case ('link'):
                 return (
-                    <Button onClick={() => navigatePage(button.href)}>
+                    <Button key={generateKey()} onClick={() => navigatePage(button.href)}>
                         {button.title}
                     </Button>
                 )
 
             case ('action'):
                 return (
-                    <Button onClick={button.action}>
+                    <Button key={generateKey()} onClick={button.action}>
                         {button.title}
                     </Button>
                 )
 
             default: 
                 return (
-                    <Button>
+                    <Button key={generateKey()}>
                         {button.title}
                     </Button>
                 )
